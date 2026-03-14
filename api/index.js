9ab52e103882081cd7e9409b05dee34692f78544
softsolspk1 <kashiffareed2022@gmail.com>
2026-03-14 22:44:05 +0500

    Debug: Bare bones root api test to isolate Vercel crash

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
-
-const app = require('../backend/server');
-module.exports = app;
+module.exports = (req, res) => {
+  res.json({ 
+    message: 'Root API is working!', 
+    timestamp: new Date(),
+    node_version: process.version 
+  });
+};
